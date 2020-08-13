d4a1dc7e8849a884c3a5fa87695ca42a47a60ca9
Matias Caporale <matiasccyo@gmail.com>
2020-08-12 21:27:59 -0300

    postgres db connected

--- FaceDetectorAPI/server.js
+++ FaceDetectorAPI/server.js
@@ -3,74 +3,86 @@ const bodyParser = require('body-parser');
 const { json } = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'admin',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 app.use(bodyParser(json()));
 app.use(cors());
-
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '1234',
-            name: 'Sally',
-            password: 'apples',
-            email: 'sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
 
 app.get('/', (req, res) =>{
     res.send('This is working');
 });
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password ) {
-        res.json(database.users[database.users.length-1]);
-    } else {
-        res.status(400).json('error loggin in')
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(async data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            try {
+                const user = await db.select('*').from('users')
+                    .where('email', '=', req.body.email);
+                res.json(user[0]);
+            }
+            catch (err) {
+                return res.status(400).json('unable to get user');
+            }
+        } else {
+            res.status(400).json('wrong credentials')
+        }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 });
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-
-    });
-    database.users.push(
-        {
-            id: '125',
-            name: name,
-            email: email,
-            password: password,
-            entries: 0,
-            joined: new Date()
-        }
-    )
-    res.json(database.users[database.users.length-1]);
+    bcrypt.hash(password, 10, function(err, hash) {
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                      })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'))
+    })
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            return res.json(user);
+    db.select('*').from('users').where({id}).then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json("no such user")
         }
     })
-    if (!found) {
-        res.status(404).json("no such user")
-    }
+    .catch(err => res.status(400).json("error getting user"))
 })
 
 app.put('/image', (req, res) => {
@@ -83,7 +95,7 @@ app.put('/image', (req, res) => {
         }
     })
     if (!found) {
-        res.status(404).json("no such user")
+        res.status(400).json("no such user")
     }
 })
 
